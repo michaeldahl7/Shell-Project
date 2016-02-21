3052bd0859860fc8f43c73553c30ae715e924bee
Michael Dahl <michaeldahl7@gmail.com>
2016-02-20 18:33:30 -0800

    moar work

--- p2.c
+++ p2.c
@@ -6,14 +6,7 @@ CS 570
 Due: 2/10/2016
  */
 #include "p2.h" /* Not sure what else ill need*/
-#include <unistd.h>
-#include <stdlib.h>
-#include "CHK.h"
-#include <time.h>
-#include <dirent.h>
-#include <stdio.h>
-#include <sys/types.h>  /* added for wait call */
-#include <sys/wait.h>   /* added for wait call */
+
 //#define statements /*such as flags , */
 //declarations;
 int c;
@@ -27,11 +20,11 @@ int wordLength;
 int leftCarrotFound = FALSE;
 int rightCarrotFound = FALSE;
 int ampersandFound = FALSE;
-char * nullptr = NULL;
 
 int main()
 {
 
+int max;
 //Declaration of locals
 //any necessary setup, including signal catcher??
 	for(;;){
@@ -43,12 +36,35 @@ int main()
 		if(wordLength == -1){
 			break;
 		}
+
+		int kidpid;
+
+		/* do the fork, remember the value returned by fork(), and test it */
+		if (-1 == (kidpid = fork())) {
+			/* -1 indicates the fork was unsuccessful */
+			perror("Cannot fork");
+			exit(1);
+		} else if (0 == kidpid) {
+			/* fork() returns 0 to the newly-created child process */
+			sleep(1);
+			/* block for 1 second, to ensure a context switch for this example */
+			printf("child says: my pid = %d, parent pid = %d\n", getpid(), getppid());
+			exit(0);
+		} else {
+/* fork() returns a positive number to the process that issued the fork()
+ * The number it returns is the PID of the child process that was just created */
+			sleep(1);
+			/* block for 1 second, to ensure a context switch for this example */
+			printf("parent says: child pid = %d, my pid = %d\n", kidpid, getpid());
+			exit(0);
+		}
 		//else if()
 		/*if(fork() == 0){
-			redirect I/O as requested
-
-			use execvp() to start requested process
-			If execvp() vails {
+			redirect I/O as requested*/
+   		//CHK(execvp(newargv[0], newargv));
+		execvp(newargv[0], newargv);
+		/*	use execvp() to start requested process
+			If execvp() fails {
 				print error message
 			exit(9);
 			}
@@ -56,9 +72,6 @@ int main()
 		//This is where i handle if ampersand true on not.
 		If approprioate, wait for child to complete;
 		Else print the childs pid, and in this case, the child should redirect its stdin to /dev/null  */
-		if(ampersandFound == true){
-
-		}
 	}
 	//killpg(getpid(), SIGTERM); //Terminate any children that are still running. WARNING: bad args to killpg can kill auto grader,
 
@@ -70,36 +83,39 @@ int main()
 void parse(){
 	ptr = s;
 	wordLength = getword(s);
-	while(wordLength != 0 && wordLength != -1){
-		if(leftCarrotFound == TRUE){
-			newargv[wordCount] = *ptr;	//this is the input file argument where to verify input..
+	while(wordLength != 0 && wordLength != -1) {
+		if (leftCarrotFound == TRUE) {
+			newargv[wordCount] = ptr;    //this is the input file argument where to verify input..
 			leftCarrotFound = FALSE;
 		}
-		if(rightCarrotFound == TRUE){
-			newargv[wordCount] = *ptr;	//this is the output file argument
+		else if (rightCarrotFound == TRUE) {
+			newargv[wordCount] = ptr;    //this is the output file argument
 			rightCarrotFound = FALSE;
 		}
-		if(*ptr == '<'){
+		else if (*ptr == '<') {
 			leftCarrotFound = TRUE;
 			printf("word found is <. \n");
-		}2
-		if(*ptr == '>'){
+		}
+		else if (*ptr == '>') {
 			rightCarrotFound = TRUE;
 			printf("word found is >. \n");
 			//handle outputfile
 		}
-		if(*ptr == '&'){
+		else if (*ptr == '&') {
 			ampersandFound = TRUE;
 			printf("word found is &. \n");
-			newargv[wordCount] = nullptr;
+			newargv[wordCount] = NULL;
 			break;
 			//set up background
 			//null pointer in newargv array
 			//end line break, or goto?
 		}
+		else {
 		//newargv[wordCount] = ptr;
 		printf("n=%d, s=[%s]\n", wordLength, ptr);
-		ptr = (ptr+(wordLength + 1));
+		ptr = (ptr + (wordLength + 1));
 		wordLength = getword(ptr);
+		}
 	}
+
 }
--- p2.h
+++ p2.h
@@ -1,6 +1,17 @@
 #include "stdio.h"
 #include "getword.h"
 #include "stdlib.h"
+#include <stdlib.h>
+#include <strings.h>
+#include <unistd.h>
+//#include "CHK.h"
+#include <time.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <sys/types.h>  /* added for wait call */
+#include <fcntl.h>
+#include <sys/stat.h>
+//#include <sys/wait.h>   /* added for wait call */
 #define MAXITEM 100 /* Max number of words per line */
 
 
